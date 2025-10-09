c0d82698adde3c2d1e99aeae9cf01adeec6bedfc
JanaBog <56586965+JanaBog@users.noreply.github.com>
2025-10-09 19:23:23 +0200

    Update pr.1.2.f90

--- 1 Voved/pr.1.2.f90
+++ 1 Voved/pr.1.2.f90
@@ -6,15 +6,16 @@ program primer1_2
   logical :: underflow_seen, overflow_seen
 
   ! ---------------------------------------------------------------
-  ! Цел:
-  !   Да се покаже што се случува со реални броеви кога ќе излезат
-  !   од опсегот на претставување (overflow и underflow).
+  ! Cel:
+  !   Da se pokaze shto se sluchuva so realni broevi koga kje izlezat
+  !   od opsegot na pretstavuvanje (overflow i underflow).
   !
-  ! Почнуваме од Y=1 и секојпат го удвојуваме (Y=Y*2).
-  ! X = 1/Y станува сè помал. Кога X ќе стане нула → UNDERFLOW.
-  ! Кога Y ќе стане бесконечен (Inf) → OVERFLOW.
+  ! Pochnuvame od y=1 i go udvojuvame y = y*2.
+  ! Togash, x = 1/y stanuva se pomal. Koga x kje stane nula - underflow. 
+  ! Koga y kje stane beskonecnhost - overflow.
   !
-  ! Што ќе се случи ако наместо real користиме real(8)? Провери. 
+  ! - Shto kje se sluchi ako namesto real koristime real(8)? Proveri.
+  ! - Shto kje bide razlichno ako y go prepolovuvame y = y/2? Proveri. 
   ! ---------------------------------------------------------------
 
   y = 1.0
@@ -33,15 +34,15 @@ program primer1_2
 
      write (*,'(I4,1X,ES14.7,1X,ES14.7)') i, x, y
 
-     ! Проверка за overflow (Y престанува да расте или станува негативен)
+     ! Proverka za overflow (y prestanuva da raste ili stanuva negativen)
      if (.not. overflow_seen .and. (y <= 0.0 .or. y/2.0 /= y_prev)) then
-        print *, '→ OVERFLOW: Y излегол од опсег на чекор', i
+        print *, 'OVERFLOW: y izlegol od opseg na chekor ', i
         overflow_seen = .true.
      end if
      
-     ! Проверка за underflow (X = 0.0)
+     ! Proverka za underflow (X = 0.0)
      if (.not. underflow_seen .and. x == 0.0) then
-        print *, '→ UNDERFLOW: X падна на нула на чекор', i
+        print *, 'UNDERFLOW: x padna na nula na chekor', i
         underflow_seen = .true.
      end if
      
